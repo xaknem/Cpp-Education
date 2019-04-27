a55f8103ddf56a36100dbf3adf72ae4aca899ae9
asmashentsev <asmashentsev@esphere.ru>
2019-04-27 15:07:28 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// выводит сообщение на экран
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "lol kek soobshenie.";
+    cout << endl;
+    cout << "novaya stroka";
+    return 0;
+}
