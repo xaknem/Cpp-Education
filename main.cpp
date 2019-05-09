662439aecf7dcbd8a65380dc12d04c7791a89f31
asmashentsev <asmashentsev@esphere.ru>
2019-05-09 21:50:02 +0300

    Начал программу для расчета индекса массы тела.

--- main.cpp
+++ main.cpp
@@ -4,19 +4,40 @@
 
 using namespace std;
 
-const int dinf = 12;
+const int inchesInFoot = 12;
+const double footsInMeters = 0.0254;
+const double foontsInKg = 2.2;
 
-int main() {
+
+int inchesToFoot() {
     int height;
     cout << "Vvedite rost v duimax: ";
     cin >> height;
     cout << "\nIdet raschet...\n";
-    int foots = height / dinf;
+    int foots = height / inchesInFoot;
     int dumes = height % foots;
     cout << "Rost " << foots << " footov, and " << dumes << " duimov";
     return 0;
 }
 
-void lolProverka() {
 
+const int zaloopa = 123;
+int main() {
+
+    double heightInFoots;
+    int heightInInches;
+    int weight;
+    cout << "Put your height in foots : \n";
+    cin >> heightInFoots;
+    cout << "And inches : \n";
+    cin >>
+    cout << "Put your weight on foonts: \n";
+    cin >> weight;
+    cout << "RUN" << endl;
+
+
+
+
+    cout << "BMI is : " << ;
+    return 0;
 }
