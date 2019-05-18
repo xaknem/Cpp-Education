6342ccd4e0ed2ff625f9a17b65e8d19159d87800
asmashentsev <asmashentsev@esphere.ru>
2019-05-19 01:56:10 +0300

    Done 12 excercises from 4 chapter

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // выводит сообщение на экран
 #include <iostream>
 #include <cmath>
+#include <array>
 
 using namespace std;
 
@@ -42,6 +43,7 @@ int bmiIndex() {
 // Geograpgic coordinates
 const double angularMinutesInDegree = 60;
 const double angularSecondsInAngularMinute = 60;
+
 int latitude() {
     double latitudeInDegrees;
     double latitudeInMinutes;
@@ -51,7 +53,8 @@ int latitude() {
     cin >> latitudeInDegrees;
     cin >> latitudeInMinutes;
     cin >> latitudeInSeconds;
-    double latitude = latitudeInDegrees + (latitudeInMinutes / angularMinutesInDegree) + latitudeInSeconds / (angularMinutesInDegree * angularSecondsInAngularMinute);
+    double latitude = latitudeInDegrees + (latitudeInMinutes / angularMinutesInDegree) +
+                      latitudeInSeconds / (angularMinutesInDegree * angularSecondsInAngularMinute);
     cout << " == " << latitude << " degrees;";
     return 0;
 }
@@ -59,9 +62,10 @@ int latitude() {
 const int secsInDays = 86400;
 const int secsInHour = 3600;
 const int secsInMins = 60;
+
 int secondsToRealTime() {
     int seconds;
-    cout << "Enter the number of seconds : " ;
+    cout << "Enter the number of seconds : ";
     cin >> seconds;
     int days;
     int hours;
@@ -71,7 +75,8 @@ int secondsToRealTime() {
     hours = seconds % secsInDays / secsInHour;
     minutes = seconds % secsInDays % secsInHour / secsInMins;
     seconds = seconds % secsInDays % secsInHour % secsInMins;
-    cout << endl << seconds << " seconds = " << days << " days, " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds.";
+    cout << endl << seconds << " seconds = " << days << " days, " << hours << " hours, " << minutes << " minutes, "
+         << seconds << " seconds.";
     return 0;
 }
 
@@ -81,31 +86,31 @@ int populationCalc() {
     cout << "Enter the worlds population: ";
     cin >> worldsPopulation;
     cout << "\nEnter the population of the US: ";
-    cin  >> countryPopulation;
+    cin >> countryPopulation;
     double percent = double(countryPopulation) / worldsPopulation * 100.0;
     cout << "The population of the US is " << percent << "% of the worlds population.";
     return 0;
 }
 
 int fuelConsumptionCalculatorUS() {
-    int  miles;
+    int miles;
     double gallons;
     cout << "Enter the road length in miles: ";
     cin >> miles;
     cout << "Enter the amount fuel spent in gallons: ";
-    cin  >> gallons;
-    int consumption = gallons / miles  * 100;
+    cin >> gallons;
+    int consumption = gallons / miles * 100;
     cout << "The fuel consumption for 100 miles is " << consumption << " gallons.";
     return 0;
 }
 
 int europeanConsimptionToUs() {
-    int  kilometers;
+    int kilometers;
     double liters;
     cout << "Enter the road length in kilometers: ";
     cin >> kilometers;
     cout << "Enter the amount fuel spent in liters: ";
-    cin  >> liters;
+    cin >> liters;
     double miles = kilometers / 100.0 * 62.14;
     double gallons = liters * 3.875;
     double consumption = miles / gallons;
@@ -113,9 +118,9 @@ int europeanConsimptionToUs() {
     return 0;
 }
 
-int main() {
+int test() {
     using namespace std;
-    double* p3 = new double[3];
+    double *p3 = new double[3];
     p3[0] = 0.2;
     p3[1] = 0.5;
     p3[2] = 0.8;
@@ -123,6 +128,81 @@ int main() {
     cout << "p3[1] is " << p3[1] << endl;
     cout << p3 + 1;
 
-    short kek [20];
-    short (*pas) [20] = &kek;
+    short kek[20];
+    short (*pas)[20] = &kek;
+    kek + 1;
+    kek - 1;
 }
+
+int fourth1and2() {
+    char actor[30];
+    short betsie[100];
+    float chuck[13];
+    long double dipsea[64];
+
+    array<char, 30> actor2;
+    array<short, 100> betsie2;
+    array<float, 13> chuck2;
+    array<long double, 64> dipsea2;
+    return 0;
+}
+
+void fourth3() {
+    int mass[5] = {1,3,5,7,9};
+}
+
+void fourth4() {
+    int mass[5] = {1,3,5,7,9};
+    int even = mass[0] + mass[4];
+    cout << even;
+}
+
+void fourth5() {
+    float ideas[2] = {1,2};
+    cout << ideas[1];
+}
+
+void fourth6and7() {
+    char some[] = "cheeseburger";
+    string newstring = "Waldorf Salad";
+}
+
+struct fish{
+    string kindof;
+    int ounce;
+    float length;
+};
+
+enum Responce2 {No, Yes ,Maybe};
+
+void fourth9() {
+    fish largest = {
+            "crucian",
+            10,
+            11.2
+    };
+
+}
+
+void fourth11() {
+    double ted = 2.4;
+
+    double* pt = &ted;
+
+    cout << *pt;
+}
+
+void fourth12() {
+    float treacle[10];
+    float* pt = treacle;
+    cout << "first element " << pt[0] << endl;
+    cout << "last element " << pt[9];
+    //or as the same
+    cout << *pt << " " << *(pt+9) << endl;
+}
+
+void fourth13() {
+
+}
+
+
